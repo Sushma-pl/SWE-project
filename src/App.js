4654c8db14c3a6e8239bf2183ecc6c47ee8bb4ca
Namita Kumari <cs20btech11034@iith.ac.in>
2023-04-12 07:30:59 +0530

    adding simplistic table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import Navbar from "./components/homePage/Navbar";
-import Carousel from "./components/homePage/Carousel";
 import Footer from "./components/homePage/Footer";
-import Contents from "./components/homePage/Contents";
 
 function App() {
   return (
@@ -12,17 +10,59 @@ function App() {
       </header>
       <main>
         <h1 className="mb-3">Welcome to Research lab IITH</h1>
-        <h3 className="mb-3">Department of computer science</h3>
+        <centre><h3 className="mb-3">Conference Deadlines</h3></centre>
+        <table class="table">
+  <thead>
+    <th>Conference</th>
+    <th>Abstract Date</th>
+    <th>Submission Date</th>
+    <th>Notification Date</th>
+    <th>Event Date</th>
+    <th>Location</th>
+    <th>Core Rank</th>
+  </thead>
+  <tbody>
+    <tr>
+      <td data-label="Conference">1</td>
+      <td data-label="Abstract Date">Dinesh</td>
+      <td data-label="Submission Date">34</td>
+      <td data-label="Notification Date">50%</td>
+      <td data-label="Event Date">Passed</td>
+      <td data-label="Location">50%</td>
+      <td data-label="Core Rank">Passed</td>
+    </tr>
 
-        <div className="container-fluid">
-          <div className="row">
-            <Carousel />
-          </div>
-          <div className="row">
-            <Contents />
-          </div>
+    <tr>
+      <td data-label="Conference">2</td>
+      <td data-label="Abstract Date">Kamal</td>
+      <td data-label="Submission Date">23</td>
+      <td data-label="Notification Date">70%</td>
+      <td data-label="Event Date">Passed</td>
+      <td data-label="Location">50%</td>
+      <td data-label="Core Rank">Passed</td>
+    </tr>
 
-        </div>
+    <tr>
+      <td data-label="Conference">3</td>
+      <td data-label="Abstract Date">Neha</td>
+      <td data-label="Submission Date">20</td>
+      <td data-label="Notification Date">90%</td>
+      <td data-label="Event Date">Passed</td>
+      <td data-label="Location">50%</td>
+      <td data-label="Core Rank">Passed</td>
+    </tr>
+
+    <tr>
+      <td data-label="Conference">4</td>
+      <td data-label="Abstract Date">Priya</td>
+      <td data-label="Submission Date">30</td>
+      <td data-label="Notification Date">30%</td>
+      <td data-label="Event Date">Failed</td>
+      <td data-label="Location">50%</td>
+      <td data-label="Core Rank">Passed</td>
+    </tr>
+  </tbody>
+</table>
       </main>
       <Footer />
     </div>
