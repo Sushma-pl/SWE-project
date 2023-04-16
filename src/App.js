9c072896ac56a23644d539509d1f4ce8f959a82e
Sushma-AI1103 <sp067611@gmail.com>
2023-04-16 14:18:06 +0530

    added change password button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from "./components/Login_page/Login"
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Homepage from "./components/homePage/Homepage";
 import ConferencesPage from "./components/Research/ConferencesPage";
+import ForgotPassword from "./components/Login_page/ForgotPassword";
+import ChangePassword from "./components/Login_page/ChangePassword";
 
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
       <Route exact path="/" element={<Homepage/>} />
       <Route exact path="/login" element={<Login/>} />
       <Route exact path="/research/conference" element={<ConferencesPage/>} />
+      <Route exact path="/login/forgot-password" element={<ForgotPassword/>} />
+      <Route exact path="/login/change-password" element={<ChangePassword/>} />
 
       {/* here we can add another route to direct to appropriate page */}
       </Routes>    
