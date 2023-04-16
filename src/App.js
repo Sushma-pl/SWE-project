d2c9807b23baf01ba2e74e1b551489ecfd5e0267
Sushma-AI1103 <sp067611@gmail.com>
2023-04-17 00:32:08 +0530

    added navbar to profile pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,13 @@ import Alumni from "./components/Members/Alumni";
 import Photos from "./components/gallery/photos";
 import Videos from "./components/gallery/videos";
 import Student from "./components/Members/Student";
+import EditNavbar from "./components/Profile/EditNavbar";
+import AddConference from "./components/Profile/AddConference";
+import PostAnnouncement from "./components/Profile/PostAnnouncement";
+import PostNews from "./components/Profile/PostNews";
+import UploadPictures from "./components/Profile/UploadPictures";
+import AddVisitor from "./components/Profile/AddVisitor";
+import AddAlumni from "./components/Profile/AddAlumni";
 
 function App() {
   return (
@@ -28,9 +35,15 @@ function App() {
       <Route exact path="/member/students" element={<Student/>} /> 
       <Route exact path="/gallery/photos" element={<Photos/>} /> 
       <Route exact path="/gallery/video" element={<Videos/>} /> 
-
       <Route exact path="/member/alumni" element={<Alumni/>} />  
       <Route exact path="/member/mentor" element={<Mentor/>} />      
+      <Route exact path="/profile" element={<EditNavbar/>} />      
+      <Route exact path="/profile/add-conference" element={<AddConference/>} />      
+      <Route exact path="/profile/post-announcement" element={<PostAnnouncement/>} />      
+      <Route exact path="/profile/post-news" element={<PostNews/>} />      
+      <Route exact path="/profile/upload-pictures" element={<UploadPictures/>} />      
+      <Route exact path="/profile/add-visitor" element={<AddVisitor/>} />      
+      <Route exact path="/profile/add-alumni" element={<AddAlumni/>} />      
 
       {/* here we can add another route to direct to appropriate page */}
       </Routes>
