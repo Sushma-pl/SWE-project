8746220cbc67d7983480579aba36158e3155d461
Sushma-AI1103 <sp067611@gmail.com>
2023-04-16 14:28:13 +0530

    removed extra code

--- src/components/Login_page/ForgotPassword.js
+++ src/components/Login_page/ForgotPassword.js
@@ -11,7 +11,6 @@ export class ForgotPassword extends Component {
         <div className="ForgotAuth-form-content">
         <h3 className="ForgotAuth-form-title">
           Forgot Password
-          {/* <i className='fas fa-user-circle icon-size'></i> */}
           </h3>
             
             <div className="form-group">
@@ -39,53 +38,7 @@ export class ForgotPassword extends Component {
               <button type="submit" className="btn btn-primary">
                 Send Reset Link
               </button>
-              {/* <label>
-              <input type="checkbox"  name="remember" /> Remember me
-              </label> */}
             </div>
-
-            {/* <h4 className='ForgotAuth-form-title'>Change password</h4> */}
-
-            {/* <div className="form-group">
-              <label>Username</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter username"
-                required
-              />
-            </div> */}
-
-            {/* <div className="form-group">
-              <label>Old Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter email"
-                required
-              />
-            </div> */}
-
-            {/* <div className="form-group">
-              <label>New Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter email"
-                required
-              />
-            </div> */}
-
-            {/* <div className="form-group">
-              <label>Re-enter New Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter email"
-                required
-              />
-            </div>  */}
-
           </div>
         </form>
       </div>
