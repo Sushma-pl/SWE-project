16420afd0452a4ba504a9cc4ec7815d7b7c54553
PrasannaLanka <cs20btech11029@iith.ac.in>
2023-04-16 10:58:59 +0530

    fixed Home

--- src/components/homePage/Homepage.js
+++ src/components/homePage/Homepage.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import Carousel from "./Carousel";
+import React, { Component } from "react";
+import CarouselComponent from "./Carousel";
 import Footer from "./Footer";
 import Contents from "./Contents";
 
@@ -8,20 +8,20 @@ export class Homepage extends Component {
     return (
       <div>
         <main>
-        <h1 className="place-holder-nav-bar">Welcome to Research lab IITH</h1>
-        <div className="container-0 mx-6" >
-          <div className="row">
-            <Carousel />
+          <h1 className="place-holder-nav-bar">Welcome to Research lab IITH</h1>
+          <div className="container-0 mx-6">
+            <div className="row">
+              <CarouselComponent />
+            </div>
+            <div className="row mx-3">
+              <Contents />
+            </div>
           </div>
-          <div className="row mx-3" >
-            <Contents />
-          </div>
-        </div>
-      </main>
-      <Footer />
+        </main>
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default Homepage
+export default Homepage;
