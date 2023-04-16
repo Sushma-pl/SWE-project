1f83fce2aede16a379365e5ffd7d25bdf5351094
Sushma-AI1103 <sp067611@gmail.com>
2023-04-16 17:44:37 +0530

    merged gallery with master

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 import Navbar from "./components/homePage/Navbar";
-import Login from "./components/Login_page/Login"
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./components/Login_page/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/homePage/Homepage";
 import ConferencesPage from "./components/Research/ConferencesPage";
 import ForgotPassword from "./components/Login_page/ForgotPassword";
 import ChangePassword from "./components/Login_page/ChangePassword";
 import Mentor from "./components/Members/Mentor";
 import Student from "./components/Members/Student";
-
-
+import Photos from "./components/gallery/photos";
+import Videos from "./components/gallery/videos";
 
 function App() {
   return (
     <Router>
-    <div className="homepage">     
+      <div className="homepage">
         <Navbar />
       <Routes>
       <Route exact path="/" element={<Homepage/>} />
@@ -23,10 +23,12 @@ function App() {
       <Route exact path="/login/forgot-password" element={<ForgotPassword/>} />
       <Route exact path="/login/change-password" element={<ChangePassword/>} />      
       <Route exact path="/member/mentor" element={<Mentor/>} />      
-      <Route exact path="/member/students" element={<Student/>} />      
+      <Route exact path="/member/students" element={<Student/>} />   
+      <Route exact path="gallery/photos" element={<Photos />} />
+      <Route exact path="gallery/video" element={<Videos />} />   
       {/* here we can add another route to direct to appropriate page */}
-      </Routes>    
-    </div>
+      </Routes>
+     </div>    
     </Router>
   );
 }
