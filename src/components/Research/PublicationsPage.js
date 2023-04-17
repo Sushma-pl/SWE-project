f95815e62956869fe23103bbca243e095f9670f6
Namita Kumari <cs20btech11034@iith.ac.in>
2023-04-17 11:12:52 +0530

    adding publications page

--- src/components/Research/PublicationsPage.js
+++ src/components/Research/PublicationsPage.js
@@ -1,65 +1,6 @@
 import "./PublicationsPage.css"
 import React, { Component } from 'react'
 import Footer from "../homePage/Footer";
-import { useState } from 'https://esm.sh/react@18.2.0';
-
-// Accordion container
-const Accordion = (props) => {
-	return (
-    <div className="accordion">
-      {props.drawer.map((drawer) =>
-        <Drawer title={drawer.title} content={drawer.content}/>
-      )}
-    </div>
-	);
-}
-
-// Accordion drawer
-const Drawer = (props) => {
-	const {title, content} = props;
-	const [isOpen, setOpen] = useState(false);
-	
-	const toggleDrawer = (e) => {
-		let content = e.target.closest('.accordion__drawer').querySelector('.accordion__content');
-		let contentInner = content.querySelector('.accordion__content-inner');
-		setOpen(!isOpen);
-		isOpen ? content.style.height = 0 : content.style.height = contentInner.offsetHeight + 'px';
-	}
-	
- 	return (
-		<div className={`accordion__drawer ${isOpen ? 'accordion__drawer--open' : '' }`}>
-			<div className="accordion__header" onClick={(e) => toggleDrawer(e)}>
-				<h3 className="accordion__title">{title}</h3>
-				<button className="accordion__toggle">{isOpen ? '-' : '+'}</button>
-			</div>
-			<div className="accordion__content">
-				<div className="accordion__content-inner">
-					{content}
-				</div>
-			</div>
-		</div>	
-    )
-}
-
-// Drawer data
-const Drawers = [
-	{
-		title: "First Law",
-		content:
-		"A robot may not injure a human being or, through inaction, allow a human being to come to harm."
-	},
-	{
-		title: "Second Law",
-		content:
-		"A robot must obey the orders given it by human beings except where such orders would conflict with the First Law."
-	},
-	{
-		title: "Third Law",
-		content:
-		"A robot must protect its own existence as long as such protection does not conflict with the First or Second Law."
-	}
-];
-
 export default class PublicationsPage extends Component {
   render() {
     return (
@@ -67,7 +8,68 @@ export default class PublicationsPage extends Component {
         <main>
           <h1 className="place-holder-nav-bar">Welcome to Research lab IITH</h1>
           <center><h3 className="mb-3 mt-5">Publications</h3></center>
-          <Accordion drawers={Drawers} />
+          <div className="accordion accordion-flush" id="accordionFlushExample">
+						<div className="accordion-item">
+							<h2 className="accordion-header" id="flush-headingOne">
+								<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+									Multi Interval DomLock (MID): Towards Improving Concurrency in Hierarchies
+								</button>
+							</h2>
+							<div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+								<div className="accordion-body"><i>Author(s): Anju M A, Rupesh Nasre<br/>Journal/Conference: ACM Transactions on Parallel Computing (TOPC), 2022</i></div>
+							</div>
+						</div>
+						<div className="accordion-item">
+							<h2 className="accordion-header" id="flush-headingTwo">
+								<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+								Certified Mergeable Replicated Data Types
+								</button>
+							</h2>
+							<div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+								<div className="accordion-body"><i>Author(s): Vimala Soundarapandian, Adharsh Kamath, Kartik Nagar, KC Sivaramakrishnan<br/>Journal/Conference: ACM Conference on Programming Language Design and Implementation (PLDI), Jun 13-17, 2022</i></div>
+							</div>
+						</div>
+						<div className="accordion-item">
+							<h2 className="accordion-header" id="flush-headingThree">
+								<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+								Hansie: Hybrid and Consensus Regression Test Prioritization
+								</button>
+							</h2>
+							<div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+								<div className="accordion-body"><i>Author(s): Shouvick Mondal, Rupesh Nasre<br/>Journal/Conference: Journal of Systems and Software (JSS), 2020 and IEEE International Conference on Software Testing, Verification and Validation (ICST), 2021</i></div>
+							</div>
+						</div>
+						<div className="accordion-item">
+							<h2 className="accordion-header" id="flush-headingFour">
+								<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+								Router Buffer Caching for Managing Shared Cache Blocks in Tiled Multi-Core Processors
+								</button>
+							</h2>
+							<div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+								<div className="accordion-body"><i>Author(s): Joe Augustine, Raghavendra Kanakagiri, John Jose, Madhu Mutyam<br/>Journal/Conference: IEEE International Conference on Computer Design (ICCD), 2020</i></div>
+							</div>
+						</div>
+						<div className="accordion-item">
+							<h2 className="accordion-header" id="flush-headingFive">
+								<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+								DisGCo: A Compiler for Distributed Graph Analytics
+								</button>
+							</h2>
+							<div id="flush-collapseFive" className="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+								<div className="accordion-body"><i>Author(s): Anchu Rajendran, V. Krishna Nandivada<br/>Journal/Conference: ACM Transactions on Architecture and Code Optimization (TACO), 2020</i></div>
+							</div>
+						</div>
+						<div className="accordion-item">
+							<h2 className="accordion-header" id="flush-headingSix">
+								<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+								Optimization of Inter Cache Traffic Entanglement in Tagless Caches with Tiling Opportunities
+								</button>
+							</h2>
+							<div id="flush-collapseSix" className="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+								<div className="accordion-body"><i>Author(s): S.R. Swamy Saranam Chongala, Sumitha George, Hariram Thirucherai Govind, Jagadish Kotra, Madhu Mutyam, John Samson, Mahmut Kandemir, Vijaykrishnan Narayanan<br/>Journal/Conference: International Conference on Compilers, Architectures, and Synthesis for Embedded Systems (CASES), Sep 20-25, 2020</i></div>
+							</div>
+						</div>
+					</div>
         </main>
         <Footer />
       </div>
